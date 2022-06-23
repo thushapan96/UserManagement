e5c7ce5151d48b916acc06cce70ae2c3d783e3eb
Kajan <rameshkajarooban@gmail.com>
2022-06-23 22:20:47 +0530

    admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -393,7 +393,7 @@
                         <!-- <a><span style="color:#d81400;font-size: 17px;">Profile</span></a> -->
 
                         <ul class="sc-sidebar-menu " style="">
-                            <li title="" class="sc-page-active page-active">
+                            <li title="" class="sc-page-active ">
                                 <a href="#">
                                     <span class="uk-nav-icon"><i class="mdi mdi-view-dashboard-variant"></i>
 
