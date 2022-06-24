217f9f7f40731e99402d443342728fef0ea0ed0a
Kajan <rameshkajarooban@gmail.com>
2022-06-24 21:08:11 +0530

    capital

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,7 +84,7 @@ class UserController extends Controller
     }
     public function create()
     {
-        return view('admin.add');
+        return view('admin.Add');
     }
     public function edit($id)
     {
