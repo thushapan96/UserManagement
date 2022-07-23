9319a35cc5525890978d22e3ebda6af0922e9cce
Kajan <rameshkajarooban@gmail.com>
2022-07-23 15:44:19 +0530

    conflict by thusa

--- resources/views/membershipAdd.blade.php
+++ resources/views/membershipAdd.blade.php
@@ -4,7 +4,7 @@
 
     @csrf
     <div class='col-md-9 ' style="margin-left:auto !important;margin-right:auto !important">
-        <h5  style="text-align:center;color:#3f6791">Registration Of Membership</h5>
+        <h5 style="text-align:center;color:#3f6791">Registration Of Membership</h5>
 
     </div>
 
@@ -30,16 +30,9 @@
                         <div class="form-group">
                             <label>Select Package </label>
                             <select class="form-control" name="package" id="package">
-                                <option value="1" {{old('package') == '1' ? 'selected':''}}>Basic For 3 Months</option>
-                                <option value="2" {{old('package') == '2' ? 'selected':''}}>Basic For 6 Months</option>
-                                <option value="3" {{old('package') == '3' ? 'selected':''}}>Basic For 12 Months</option>
-                                <option value="4" {{old('package') == '4' ? 'selected':''}}>Standard For 3 Months</option>
-                                <option value="5" {{old('package') == '5' ? 'selected':''}}>Standard For 6 Months</option>
-                                <option value="6" {{old('package') == '6' ? 'selected':''}}>Standard For 12 Months</option>
-                                <option value="7" {{old('package') == '7' ? 'selected':''}}>Premium For 3 Months</option>
-                                <option value="8" {{old('package') == '8' ? 'selected':''}}>Premium For 6 Months</option>
-                                <option value="9" {{old('package') == '9' ? 'selected':''}}>Premium For 12 Months</option>
-
+                                @foreach($packagePriceList as $row)
+                                <option value="{{$row->id}}" {{old('package') == $row->id ? 'selected' :''}}>{{$row->type == 'Stanadard' ? 'Standard':$row->type}} For {{$row->duration}} Months - {{$row->price}} {{$currencyPrice}}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
@@ -107,9 +100,9 @@
                     <br>
                 </div>
                 <div class="d-flex">
-                <button type="button" id="ES" class="sc-button sc-button-primary sc-js-button-wave-light uk-width-1-3 waves-effect waves-button waves-light">Extend Services</button><br><br>
+                    <button type="button" id="ES" class="sc-button sc-button-primary sc-js-button-wave-light uk-width-1-3 waves-effect waves-button waves-light">Extend Services</button><br><br>
 
-                <button type="submit" class="ml-auto client-btn bmenu uk-button uk-button-primary">Register </button>
+                    <button type="submit" class="ml-auto client-btn bmenu uk-button uk-button-primary">Register </button>
                 </div>
                 <input type="text" value="" id="is_service" name="is_service" hidden>
             </fieldset>
